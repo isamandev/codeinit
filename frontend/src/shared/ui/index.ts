83b68a2af0f9dd79e82cc580202ab2614403d49e
Saman Abbasi <79277585+isamandev@users.noreply.github.com>
2026-07-19 02:53:47 +0330

    ✨ feat: add new shadcn-based UI component library

--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -1,3 +1,26 @@
-export { default as Button } from "./button/Button";
-export { default as Table } from "./table/Table";
-export { default as DraggableScroller } from "./draggable-scroller/DraggableScroller";
+export { Button, buttonVariants } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "./card";
+export { Alert, AlertTitle, AlertDescription, AlertAction } from "./alert";
+export { Separator } from "./separator";
+export {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldSeparator,
+  FieldSet,
+  FieldLegend,
+  FieldTitle,
+  FieldContent,
+} from "./field";
